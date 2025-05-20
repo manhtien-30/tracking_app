959d47ce61ceb67db2bc204053f479a771077fb8
Đỗ Mạnh Tiến <tien310001@gmail.com>
2025-05-20 12:02:30 +0700

    init project

---
+++ src/main/java/org/example/testing_api_server/TestingApiServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.testing_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/testing_api_server/TestingApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testing_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
