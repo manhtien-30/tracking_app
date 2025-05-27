2d7caf524810131bea9509eda0da00cd7404fd00
Đỗ Mạnh Tiến <tien310001@gmail.com>
2025-05-27 14:09:38 +0700

    update: config secutiry and server

---
+++ src/main/java/org/example/testing_api_server/repositories/CustomSecurityContextRepository.java
@@ -0,0 +1,7 @@
+package org.example.testing_api_server.repositories;
+
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+
+public class CustomSecurityContextRepository  {
+
+}
