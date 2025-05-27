d110f05217df88eeb4807a8870daa5ea300cccd3
Đỗ Mạnh Tiến <tien310001@gmail.com>
2025-05-27 14:09:44 +0700

    update: config secutiry and server

--- src/main/java/org/example/testing_api_server/configuration/SecurityConfig.java
+++ src/main/java/org/example/testing_api_server/configuration/SecurityConfig.java
@@ -1,20 +1,9 @@
 package org.example.testing_api_server.configuration;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
